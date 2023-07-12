36c81cf575df01cd6a2ede81263a352c4054e754
karishma-c <karishmadgct@gmail.com>
2023-07-12 20:52:25 +0530

    created new directory styles to add global styles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6b0d783e05ecac89"; }
+/******/ 		__webpack_require__.h = function() { return "d700b3ae8fb3248b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "0564063559fb0312"; }
+/******/ 	__webpack_require__.h = function() { return "76fa449db820116c"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
@@ -82,6 +82,11 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 		};
 /******/ 	};
 /******/ }
+/******/ 
+/******/ /* webpack/runtime/nonce */
+/******/ !function() {
+/******/ 	__webpack_require__.nc = undefined;
+/******/ }();
 /******/ 
 /******/ }
 );
